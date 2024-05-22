7637edd5ca442f5ef6a12e0298f5bb0030bf06dd
Eyyan1 <muhammadsufian002@gmail.com>
2024-05-23 00:56:16 +0800

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = '28096295eb1edd766e38417af95306a0';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
